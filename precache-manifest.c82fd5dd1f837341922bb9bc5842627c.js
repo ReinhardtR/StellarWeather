33d350694c9dbb8d0cfc41abc8dc042bee0cb4fa
Reinhardt Rijna <reinhardtrijna@gmail.com>
2020-10-08 14:30:00 +0200

    Updates

---
+++ precache-manifest.c82fd5dd1f837341922bb9bc5842627c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a530d3ddcbd20cfd4692c89c33530a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e14fca245aeae9a6cf22",
+    "url": "/static/css/main.98d2e4b3.chunk.css"
+  },
+  {
+    "revision": "57f0098675e9d47cd95a",
+    "url": "/static/js/2.69399ad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69399ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e14fca245aeae9a6cf22",
+    "url": "/static/js/main.8c7df36a.chunk.js"
+  },
+  {
+    "revision": "d89ae92fc598edbb697f",
+    "url": "/static/js/runtime-main.a787cda0.js"
+  },
+  {
+    "revision": "05fc925196804df8d946502d17eb7691",
+    "url": "/static/media/Gradient.05fc9251.png"
+  },
+  {
+    "revision": "74fa24c05288411f6fbd00d18cbe8c69",
+    "url": "/static/media/Logo.74fa24c0.png"
+  },
+  {
+    "revision": "4a6ca1af684adf9b821975c15d4c8029",
+    "url": "/static/media/feelsLike.4a6ca1af.png"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
